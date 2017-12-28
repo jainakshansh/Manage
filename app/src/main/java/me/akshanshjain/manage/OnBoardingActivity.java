3b8a22911c5db921aecb750e62f024716d1f3a59
Akshansh Jain <akshanshjn9@gmail.com>
2017-12-28 14:23:03 +0530

    Splash Activity added.

--- app/src/main/java/me/akshanshjain/manage/OnBoardingActivity.java
+++ app/src/main/java/me/akshanshjain/manage/OnBoardingActivity.java
@@ -1,13 +1,19 @@
 package me.akshanshjain.manage;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class OnBoardingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Setting up the activity for full screen mode.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_on_boarding);
     }
 }
