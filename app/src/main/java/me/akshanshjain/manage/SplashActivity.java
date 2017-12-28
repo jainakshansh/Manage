9143325a9f610c769b78ec7d2113fe54609cbaee
Akshansh Jain <akshanshjn9@gmail.com>
2017-12-28 22:07:25 +0530

    Added introslides and gradients

--- app/src/main/java/me/akshanshjain/manage/SplashActivity.java
+++ app/src/main/java/me/akshanshjain/manage/SplashActivity.java
@@ -1,13 +1,38 @@
 package me.akshanshjain.manage;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private TextView splashText;
+    private Typeface quicksand_b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Setting up the activity for full screen mode.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+        splashText = findViewById(R.id.splash_text);
+        quicksand_b = Typeface.createFromAsset(getAssets(), "fonts/Quicksand_Bold.ttf");
+        splashText.setTypeface(quicksand_b);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), OnBoardingActivity.class));
+                finish();
+            }
+        }, 2000);
     }
 }
