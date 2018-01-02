2c668c0dc9103d09f9cf03c64a7724b616cad99d
Akshansh Jain <akshanshjn9@gmail.com>
2018-01-02 23:29:18 +0530

    Drawer completed.

--- app/src/main/java/me/akshanshjain/manage/LandingActivity.java
+++ app/src/main/java/me/akshanshjain/manage/LandingActivity.java
@@ -1,13 +1,55 @@
 package me.akshanshjain.manage;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class LandingActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        //Setting up the toolbar for the activity.
+        toolbar = findViewById(R.id.toolbar_landing);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("");
+        }
+
+        //Configuring the Drawer Layout.
+        drawerLayout = findViewById(R.id.drawer_layout_landing);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
+        toggle.syncState();
+
+        //Configuring navigation view item clicks.
+        navigationView = findViewById(R.id.navigation_view_landing);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        //checking if drawer layout is open when back pressed and closing it first before you can close the app.
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
