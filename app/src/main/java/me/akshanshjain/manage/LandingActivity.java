8039b512d0930ebba70130300fcedf39fc5556d4
Akshansh Jain <akshanshjn9@gmail.com>
2018-01-06 13:53:27 +0530

    Switched from Hamburger to Bottom Navigation

--- app/src/main/java/me/akshanshjain/manage/LandingActivity.java
+++ app/src/main/java/me/akshanshjain/manage/LandingActivity.java
@@ -2,10 +2,8 @@ package me.akshanshjain.manage;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -13,8 +11,8 @@ import android.view.MenuItem;
 public class LandingActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
-    private DrawerLayout drawerLayout;
-    private NavigationView navigationView;
+    private BottomNavigationView bottomNavigationView;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,28 +26,19 @@ public class LandingActivity extends AppCompatActivity {
             getSupportActionBar().setTitle("");
         }
 
-        //Configuring the Drawer Layout.
-        drawerLayout = findViewById(R.id.drawer_layout_landing);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
-        toggle.syncState();
+        bottomNavigationView = findViewById(R.id.bottom_nav_landing);
+        viewPager = findViewById(R.id.view_pager_container);
 
-        //Configuring navigation view item clicks.
-        navigationView = findViewById(R.id.navigation_view_landing);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                return false;
+                switch (item.getItemId()) {
+                    case R.id.overview:
+                        viewPager.setCurrentItem(1);
+                        break;
+                }
+                return true;
             }
         });
-    }
-
-    @Override
-    public void onBackPressed() {
-        //checking if drawer layout is open when back pressed and closing it first before you can close the app.
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
     }
 }
