4787c785ad79bfc552e2493d41bb2b80edd88f4c
Akshansh Jain <akshanshjn9@gmail.com>
2018-01-13 13:45:52 +0530

    Calling cursor for transactions list

--- app/src/main/java/me/akshanshjain/manage/TransactionsActivity.java
+++ app/src/main/java/me/akshanshjain/manage/TransactionsActivity.java
@@ -1,16 +1,23 @@
 package me.akshanshjain.manage;
 
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ListView;
 
 import me.akshanshjain.manage.Adapters.ExpenseCursorAdapter;
+import me.akshanshjain.manage.Databases.ExpenseContract.ExpenseEntry;
 
-public class TransactionsActivity extends AppCompatActivity {
+public class TransactionsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private Toolbar toolbar;
     private ListView listView;
+    private View emptyView;
 
     private static final int EXPENSE_LOADER = 9;
     private ExpenseCursorAdapter expenseCursorAdapter;
@@ -39,5 +46,61 @@ public class TransactionsActivity extends AppCompatActivity {
         //List view which will be populated by the data from the database.
         listView = findViewById(R.id.list_view_transactions);
         listView.setAdapter(expenseCursorAdapter);
+
+        /*
+        Setting an empty view on the list when the transactions haven't yet been loaded from the database.
+         */
+        emptyView = findViewById(R.id.empty_view_transactions);
+        listView.setEmptyView(emptyView);
+
+        //Starting the loader to read the database information.
+        getLoaderManager().initLoader(EXPENSE_LOADER, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        /*
+        Defining the projection that specifies the columns from the table.
+         */
+        String[] projection = {
+                ExpenseEntry._ID,
+                ExpenseEntry.EXPENSE_TITLE,
+                ExpenseEntry.EXPENSE_TYPE,
+                ExpenseEntry.EXPENSE_AMOUNT,
+                ExpenseEntry.EXPENSE_CATEGORY,
+                ExpenseEntry.EXPENSE_DATE_TIME
+        };
+
+        /*
+        This loader will execute the Content Provider's query method on a background thread.
+         */
+        return new CursorLoader(this,
+                ExpenseEntry.CONTENT_URI,
+                projection,
+                null,
+                null,
+                ExpenseEntry._ID + " DESC");
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        /*
+        Updating the Expense Cursor Adapter data with the new content to bind data to views.
+         */
+        expenseCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        /*
+        Callback to when the retrieved temporary data need to be lost.
+         */
+        expenseCursorAdapter.swapCursor(null);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 }
