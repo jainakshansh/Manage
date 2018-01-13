b850cb2a1e230176ceb9540958b7e103eccb6da1
Akshansh Jain <akshanshjn9@gmail.com>
2018-01-13 22:31:57 +0530

    Added permissions logic on FAB

--- app/src/main/java/me/akshanshjain/manage/LandingActivity.java
+++ app/src/main/java/me/akshanshjain/manage/LandingActivity.java
@@ -1,13 +1,23 @@
 package me.akshanshjain.manage;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LandingActivity extends AppCompatActivity {
 
@@ -27,6 +37,13 @@ public class LandingActivity extends AppCompatActivity {
      */
     private static final int PERMISSION_CALLBACK_CONSTANT = 9;
     private static final int REQUEST_PERMISSION_SETTINGS = 7;
+    private String[] permissionsRequired = new String[]{
+            Manifest.permission.CAMERA,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    private boolean sentToSettings = false;
+    private SharedPreferences permissionStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +52,8 @@ public class LandingActivity extends AppCompatActivity {
 
         //Initialising all the views from the XML to style and add listeners to them.
         initViews();
+
+        permissionStatus = getSharedPreferences("permissionStatus", MODE_PRIVATE);
     }
 
     private void initViews() {
@@ -42,14 +61,19 @@ public class LandingActivity extends AppCompatActivity {
         quicksand_bold = Typeface.createFromAsset(getAssets(), "fonts/Quicksand_Bold.ttf");
         quicksand_medium = Typeface.createFromAsset(getAssets(), "fonts/Quicksand_Medium.ttf");
 
-        //Referencing views from the XML and styling them in the layout.
+        /*
+        Referencing views from the XML and styling them in the layout.
+         */
+
+        //Checks for permissions and opens the activity if provided.
         fab = findViewById(R.id.fab_landing);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), NewExpenseActivity.class));
+                permissionCodeLogic();
             }
         });
+
         overViewText = findViewById(R.id.overview_text);
         overViewText.setTypeface(quicksand_bold);
         overviewAmount = findViewById(R.id.overview_amount);
@@ -85,5 +109,112 @@ public class LandingActivity extends AppCompatActivity {
         expenseHeader.setTypeface(quicksand_medium);
         expenseAmount = findViewById(R.id.expense_amount);
         expenseAmount.setTypeface(quicksand_bold);
+    }
+
+    private void permissionCodeLogic() {
+        if (ContextCompat.checkSelfPermission(LandingActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                + ContextCompat.checkSelfPermission(LandingActivity.this, Manifest.permission.CAMERA)
+                + ContextCompat.checkSelfPermission(LandingActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(LandingActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    || ActivityCompat.shouldShowRequestPermissionRationale(LandingActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                    || ActivityCompat.shouldShowRequestPermissionRationale(LandingActivity.this, Manifest.permission.CAMERA)) {
+                Snackbar.make(findViewById(android.R.id.content),
+                        "Please Grant Permissions",
+                        Snackbar.LENGTH_INDEFINITE).setAction("ENABLE",
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ActivityCompat.requestPermissions(LandingActivity.this,
+                                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                                Manifest.permission.CAMERA,
+                                                Manifest.permission.READ_EXTERNAL_STORAGE},
+                                        REQUEST_PERMISSIONS);
+                            }
+                        }).show();
+            } else {
+                ActivityCompat.requestPermissions(LandingActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                Manifest.permission.READ_EXTERNAL_STORAGE,
+                                Manifest.permission.CAMERA},
+                        REQUEST_PERMISSIONS);
+            }
+        } else {
+            /*
+            Calling the function to be performed if the permissions have already been provided.
+             */
+            proceedAfterPermission();
+        }
+    }
+
+    private void proceedAfterPermission() {
+        startActivity(new Intent(getApplicationContext(), NewExpenseActivity.class));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_CALLBACK_CONSTANT) {
+            //Checking if all permissions are granted.
+            boolean allGranted = false;
+            for (int grantResult : grantResults) {
+                if (grantResult == PackageManager.PERMISSION_GRANTED) {
+                    allGranted = true;
+                } else {
+                    allGranted = false;
+                    break;
+                }
+            }
+
+            if (allGranted) {
+                proceedAfterPermission();
+            } else if (ActivityCompat.shouldShowRequestPermissionRationale(LandingActivity.this, permissionsRequired[0])
+                    || ActivityCompat.shouldShowRequestPermissionRationale(LandingActivity.this, permissionsRequired[1])
+                    || ActivityCompat.shouldShowRequestPermissionRationale(LandingActivity.this, permissionsRequired[2])) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(LandingActivity.this);
+                builder.setTitle("Need Multiple Permissions");
+                builder.setMessage("This app needs Camera and Storage Permissions.");
+                builder.setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                        ActivityCompat.requestPermissions(LandingActivity.this, permissionsRequired, PERMISSION_CALLBACK_CONSTANT);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+            } else {
+                Toast.makeText(getBaseContext(), "Unable to get Permissions", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_PERMISSION_SETTINGS) {
+            if (ActivityCompat.checkSelfPermission(LandingActivity.this, permissionsRequired[0]) == PackageManager.PERMISSION_GRANTED) {
+                //After getting the permissions.
+                proceedAfterPermission();
+            }
+        }
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        if (sentToSettings) {
+            if (ActivityCompat.checkSelfPermission(LandingActivity.this, permissionsRequired[0]) == PackageManager.PERMISSION_GRANTED) {
+                /*
+                After getting the permissions.
+                 */
+                proceedAfterPermission();
+            }
+        }
     }
 }
