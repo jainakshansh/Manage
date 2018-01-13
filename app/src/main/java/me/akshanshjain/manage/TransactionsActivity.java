e7fea683d00252eaff9e77f8c8e23829af087d5a
Akshansh Jain <akshanshjn9@gmail.com>
2018-01-13 13:28:21 +0530

    Empty Transaction UI added

--- app/src/main/java/me/akshanshjain/manage/TransactionsActivity.java
+++ app/src/main/java/me/akshanshjain/manage/TransactionsActivity.java
@@ -3,10 +3,17 @@ package me.akshanshjain.manage;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ListView;
+
+import me.akshanshjain.manage.Adapters.ExpenseCursorAdapter;
 
 public class TransactionsActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private ListView listView;
+
+    private static final int EXPENSE_LOADER = 9;
+    private ExpenseCursorAdapter expenseCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +29,15 @@ public class TransactionsActivity extends AppCompatActivity {
             getSupportActionBar().setTitle("All Transactions");
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        /*
+        Defining the Cursor Adapter for the expenses.
+        Passing in null until the loader finishes retrieving the data.
+         */
+        expenseCursorAdapter = new ExpenseCursorAdapter(this, null);
+
+        //List view which will be populated by the data from the database.
+        listView = findViewById(R.id.list_view_transactions);
+        listView.setAdapter(expenseCursorAdapter);
     }
 }
