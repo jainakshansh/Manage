c1a879826192804345a73b0a84129c24d1211a7e
Akshansh Jain <akshanshjn9@gmail.com>
2018-01-13 15:22:36 +0530

    Added New Expense Activity

---
+++ app/src/main/java/me/akshanshjain/manage/NewExpenseActivity.java
@@ -0,0 +1,13 @@
+package me.akshanshjain.manage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewExpenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_expense);
+    }
+}
