eb827e8467d0abd5182553a25d5066cc11b444f3
Akshansh Jain <akshanshjn9@gmail.com>
2018-01-26 14:38:53 +0530

    Added layout to add new card

---
+++ app/src/main/java/me/akshanshjain/manage/NewCardActivity.java
@@ -0,0 +1,13 @@
+package me.akshanshjain.manage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_card);
+    }
+}
